bd27fe6180b08f692c3f913a27c91b50e0538638
Ageers <Ageers@mail.ru>
2025-09-05 20:43:34 +0300

    task 8.8

--- src/Main.java
+++ src/Main.java
@@ -16,9 +16,9 @@ public class Main {
             }
             System.out.println();
         }
-*/
 
-        //
+
+        // 8.5 zlatopolsky
         int sum = 0;
         for (int i = 1; i <= 9; i++){
             for (int j = 1; j <= 9; j++){
@@ -27,5 +27,20 @@ public class Main {
             }
             System.out.println();
         }
+
+        */
+
+        // 8.8
+
+        int pows = 1;
+        for (int i = 1; i <= 9; i++){
+            for (int j = 1; j <= 9; j++){
+                pows = i * j;
+                System.out.print(" " + i + " X " + j + " = " + pows + "   ");
+            }
+            System.out.println();
+        }
+
+
     }
 }
