6ef3a33dc91ae8bd5b3dc594531247534cf7a6d0
Ageers <Ageers@mail.ru>
2025-09-05 20:40:47 +0300

    task 8.5

--- src/Main.java
+++ src/Main.java
@@ -19,10 +19,11 @@ public class Main {
 */
 
         //
-
-        for (int i = 0; i <= 9; i++){
-            for (int j = 0; j <= 9; j++){
-                System.out.print(i+j + "   ");
+        int sum = 0;
+        for (int i = 1; i <= 9; i++){
+            for (int j = 1; j <= 9; j++){
+                sum = i + j;
+                System.out.print(" " + i + " + " + j + " = " + sum + "   ");
             }
             System.out.println();
         }
