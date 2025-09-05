43efadb3f9a0a7338c3a5291efb2089b4776a521
Ageers <Ageers@mail.ru>
2025-09-05 20:30:26 +0300

    Initial commit// cheese desk

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                System.out.print(((i + j) % 2 == 0 ? '#' : '.'));
+            }
+            System.out.println();
+        }
+
+    }
+}
